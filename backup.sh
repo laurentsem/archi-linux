63799b90e8a7cccb11b7e8e450fa39dc159bd42b
echalet <emerickchalet@gmail.com>
2021-04-29 13:14:11 +0000

    get table done

--- backup.sh
+++ backup.sh
@@ -4,10 +4,10 @@
 #On verifie que notre fichier de destination temporaire exist
 DOSSIER=/home/save_BD
 if [ -d ${DOSSIER} ]; then
-	rm -rf ${DOSSIER}
+	sudo rm -rf ${DOSSIER}
 	echo "Le dossier existe déja, nous l'effacons"
 fi
-mkdir ${DOSSIER}
+sudo mkdir ${DOSSIER}
 echo "Le dossier ${DOSSIER} est crée"
 # On liste nos bases de données
 
@@ -16,10 +16,10 @@ for BDD in $LISTEBDD; do
 	# Exclusion des BDD information_schema , mysql et Database
 	if [[ $BDD != "information_schema" ]] && [[ $BDD != "mysql" ]] && [[ $BDD != "Database" ]] && [[ $BDD != "sys" ]] && [[ $BDD != "performance_schema" ]];  then
 		# Emplacement du dossier ou nous allons stocker les bases de données, un dossier par base de données
-		#  CHEMIN=/home/user/save_BD/$BDD
+		  CHEMIN=/home/save_BD/$BDD
+		  sudo mkdir $CHEMIN
 		  # On backup notre base de donnees
-		 #   mysqldump -u backup --single-transaction --password= $BDD > "$CHEMIN/$BDD.sql"
-		  #	  echo "|Sauvegarde de la base de donnees $BDD.sql ";
-		  echo $BDD
+		    sudo mysqldump -u root $BDD -p'codingfactory' > $CHEMIN/$BDD.sql
+		    echo "|Sauvegarde de la base de donnees $BDD.sql ";
 	fi
 done

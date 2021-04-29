a06b7338e9fdd998857283e4285bbe4d881d06e6
root <hugo.monnerie@edu.itescia.fr>
2021-04-29 20:54:34 +0000

    monitor website done

--- monitor_website.sh
+++ monitor_website.sh
@@ -11,27 +11,30 @@ done
 if [ -e /var/log/apache2/access.$SERVERWEB.log ]; then
 	> /var/log/apache2/access.$SERVERWEB.log
 	trap "iptables -F && rm /var/log/apache2/test.local.ipserroraccess.log && echo ' reset firewall' && exit" 2 15
+
+	tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | while read -r line ; do 
 	
-	error='"POST /wp-login.php HTTP/1.1" 200'
+		error='"POST /wp-login.php HTTP/1.1" 200'
 
-	#IPSERRORACCESS=`grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1`
+		IPSERRORACCESS=`grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1`
 
-       	#echo "$IPSERRORACCESS" > /var/log/apache2/$SERVERWEB.ipserroraccess.log
+       		echo "$IPSERRORACCESS" > /var/log/apache2/$SERVERWEB.ipserroraccess.log
 	
-	#echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log
+		echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log
 
-	#NUMERRORACCESSBYIP=`cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-	#if [ $NUMERRORACCESSBYIP -ge 3 ]; then
+		NUMERRORACCESSBYIP=`cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
+		if [ $NUMERRORACCESSBYIP -ge 3 ]; then
+			IPBAN=`cut -d' ' -f2 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
+			echo "try number = $NUMERRORACCESSBYIP / ip ban = $IPBAN"
+			iptables -A INPUT -s $IPBAN -j DROP
+		fi
+	done
+
+	#tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1 /var/log/apache2/access.$SERVERWEB.log | echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log | if [ `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` -ge 3 ]; then
 	#	IPBAN=`cut -d' ' -f2 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-	#	echo "try number = $NUMERRORACCESSBYIP / ip ban = $IPBAN"
+	#	echo "try number = `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` / ip ban = $IPBAN"
 	#	iptables -A INPUT -s $IPBAN -j DROP
 	#fi
-
-	tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1 /var/log/apache2/access.$SERVERWEB.log | echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log | if [ `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` -ge 3 ]; then
-		IPBAN=`cut -d' ' -f2 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-		echo "try number = `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` / ip ban = $IPBAN"
-		iptables -A INPUT -s $IPBAN -j DROP
-	fi
 else
 	echo "file access.$SERVERWEB.log not exist"
 fi

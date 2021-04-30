c05ed8fcd3b584928857d7a726cb6f5c1e5870a0
root <hugo.monnerie@edu.itescia.fr>
2021-04-30 12:27:51 +0000

    add func in monitor_website and clean variables

--- monitor_website.sh
+++ monitor_website.sh
@@ -4,36 +4,54 @@
 while getopts 'd:' c
 do
   case $c in
-    d) SERVERWEB=$OPTARG ;;
+    d) domain_name=$OPTARG ;;
   esac
 done
 
-if [ -e /var/log/apache2/access.$SERVERWEB.log ]; then
-	> /var/log/apache2/access.$SERVERWEB.log
-	trap "iptables -F && rm /var/log/apache2/test.local.ipserroraccess.log && echo ' reset firewall'" 2 15
+log_server_web="/var/log/apache2/access.$domain_name.log"
+log_ips_error_access="/var/log/apache2/$domain_name.ipserroraccess.log"
+error_str='"POST /wp-login.php HTTP/1.1" 200'
 
-	tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | while read line ; do
+reset () {
+	log_ips_error_access=$1
+	iptables -F && rm $log_ips_error_access && echo ' reset firewall'
+}
+
+banip () {
+	IPBAN=$1
+	num_error_access_by_ip=$2
+	echo "ip ban: $IPBAN / try fail: $num_error_access_by_ip"
+	iptables -A INPUT -s $IPBAN -j DROP
+}
+
+startfirewall () {
+	> $log_server_web
+	trap "reset $log_ips_error_access" 2 15
+
+	tail -fn 10 $log_server_web | while read line ; do
 		echo "${line}"
-		error='"POST /wp-login.php HTTP/1.1" 200'
 
-		IPSERRORACCESS=`grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1`
+		ip_error_access=`grep "$error_str" $log_server_web | cut -d' ' -f1`
 
-       		echo "$IPSERRORACCESS" > /var/log/apache2/$SERVERWEB.ipserroraccess.log
+       		echo "$ip_error_access" > $log_ips_error_access
 	
-		echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log
+		echo `sort $log_ips_error_access | uniq -c` > $log_ips_error_access
 
-		NUMERRORACCESSBYIP=`cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-		if [ $NUMERRORACCESSBYIP -ge 3 ]; then
-			IPBAN=`cut -d' ' -f2 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-			echo "try number = $NUMERRORACCESSBYIP / ip ban = $IPBAN"
-			iptables -A INPUT -s $IPBAN -j DROP
+		num_error_access_by_ip=`cut -d' ' -f1 $log_ips_error_access`
+		if [ $num_error_access_by_ip -ge 3 ]; then
+			IPBAN=`cut -d' ' -f2 $log_ips_error_access`
+			banip $IPBAN $num_error_access_by_ip
 		fi
 	done
+}
+
+if [ -e $log_server_web ]; then
+	startfirewall
 else
-	if [ -z $SERVERWEB ]; then
+	if [ -z $domain_name ]; then
 		echo "don't forget it's: ./monitor_website -d <domain_name.local>"
 	else
-		echo "file access.$SERVERWEB.log not exist"
+		echo "file access.$domain_name.log not exist"
 	fi
 fi
 

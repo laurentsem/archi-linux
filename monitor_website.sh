7754a609dd79c4365606793dfcf21cc3e69f410f
root <hugo.monnerie@edu.itescia.fr>
2021-04-30 08:53:43 +0000

    fix echo monitor_website

--- monitor_website.sh
+++ monitor_website.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # monitor_website.sh
 
-while getopts 's:' c
+while getopts 'd:' c
 do
   case $c in
-    s) SERVERWEB=$OPTARG ;;
+    d) SERVERWEB=$OPTARG ;;
   esac
 done
 
 if [ -e /var/log/apache2/access.$SERVERWEB.log ]; then
 	> /var/log/apache2/access.$SERVERWEB.log
-	trap "iptables -F && rm /var/log/apache2/test.local.ipserroraccess.log && echo ' reset firewall' && exit" 2 15
+	trap "iptables -F && rm /var/log/apache2/test.local.ipserroraccess.log && echo ' reset firewall'" 2 15
 
-	tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | while read -r line ; do 
-	
+	tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | while read line ; do
+		echo "${line}"
 		error='"POST /wp-login.php HTTP/1.1" 200'
 
 		IPSERRORACCESS=`grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1`
@@ -29,13 +29,11 @@ if [ -e /var/log/apache2/access.$SERVERWEB.log ]; then
 			iptables -A INPUT -s $IPBAN -j DROP
 		fi
 	done
-
-	#tail -fn 10 /var/log/apache2/access.$SERVERWEB.log | grep "$error" /var/log/apache2/access.$SERVERWEB.log | cut -d' ' -f1 /var/log/apache2/access.$SERVERWEB.log | echo `sort /var/log/apache2/$SERVERWEB.ipserroraccess.log | uniq -c` > /var/log/apache2/$SERVERWEB.ipserroraccess.log | if [ `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` -ge 3 ]; then
-	#	IPBAN=`cut -d' ' -f2 /var/log/apache2/$SERVERWEB.ipserroraccess.log`
-	#	echo "try number = `cut -d' ' -f1 /var/log/apache2/$SERVERWEB.ipserroraccess.log` / ip ban = $IPBAN"
-	#	iptables -A INPUT -s $IPBAN -j DROP
-	#fi
 else
-	echo "file access.$SERVERWEB.log not exist"
+	if [ -z $SERVERWEB ]; then
+		echo "don't forget it's: ./monitor_website -d <domain_name.local>"
+	else
+		echo "file access.$SERVERWEB.log not exist"
+	fi
 fi
 
